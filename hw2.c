d02e1bd0d7ba44d8e70d23fb8e8264fcb268e600
pecorium <pecorium@gmail.com>
2019-06-04 19:43:25 +0900

    Fix

--- hw2.c
+++ hw2.c
@@ -4,12 +4,13 @@ void mul(const int A[3][3], const int B[3][3], int C[3][3])
 {
     int i, j, k;
     
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 3; i++){
         for (j = 0; j < 3; j++) {
             C[i][j] = 0;
             for (k = 0; k < 3; k++)
                 C[i][j] += A[i][k] * B[k][j];
         }
+    }
 }
 
 int main(void)
